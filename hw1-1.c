9747f24dd61c3a1f249910635724c07e79da9a21
NightFeather <ss900328@gmail.com>
2014-10-09 19:36:12 +0800

    Rewrite the logic about the prevention of DivZero
    I notice the useless combination of forloop and switch,
    so I rewrite it.

--- hw1-1.c
+++ hw1-1.c
@@ -4,28 +4,34 @@
 int main(void){
 
 	float left = 0.0, right = 0.0;
+        int flag = 0, i = 0;
 
-	printf("請輸入兩個數字: ");
-	scanf(" %f %f",&left,&right);
+	printf( "請輸入兩個數字: " );
+	scanf( " %f %f", &left, &right );
+        if( right == 0.0 ) { flag = 1; }
 
-	printf("\n計算結果:\n");
-	for(int i=0;i<4;i++)
+	printf( "\n計算結果:\n" );
+	for( i = 0; i < 4; i++ )
 	{
-		switch(i){
+                if( !flag ){
+ 		 printf( "%.2f + %.2f = ", left, right);
+                }
+
+		switch( i ){
 		case 0:
-			printf("%.2f + %.2f = %.2f",left ,right , left + right);
+			printf("%.2f", left + right);
 			break;
 		case 1:
-			printf("%.2f - %.2f = %.2f",left ,right , left - right);
+			printf("%.2f", left - right);
 			break;
 		case 2:
-			printf("%.2f * %.2f = %.2f",left ,right , left * right);
+			printf("%.2f", left * right);
 			break;
 		case 3:
-			if( right == 0.0 )
+			if( flag )
 			 { printf("除數不得為零!!!"); }
 			else
-			 { printf("%.2f / %.2f = %.2f",left ,right , left / right); }
+			 { printf("%.2f", left / right); }
 			break;
 		}
 		printf("\n");
