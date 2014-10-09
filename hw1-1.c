fd62a2558a5c5180472a7f92480616dc376bb890
NightFeather <ss900328@gmail.com>
2014-10-10 02:32:16 +0800

    Change the way of result string compositing.
    Assign all parts first
    And compoite in this  `left op right = result` form at last

--- hw1-1.c
+++ hw1-1.c
@@ -3,40 +3,45 @@
 
 int main(void){
 
-	float left = 0.0, right = 0.0;
-        int flag = 0, i = 0;
+  float left = 0.0, right = 0.0, result = 0.0;
+  int flag = 0, i = 0;
+  char op = 0;
 
-	printf( "請輸入兩個數字: " );
-	scanf( " %f %f", &left, &right );
-        if( right == 0.0 ) { flag = 1; }
+  printf( "請輸入兩個數字: " );
+  scanf( " %f %f", &left, &right );
+  if( right == 0.0 ) { flag = 1; }
 
-	printf( "\n計算結果:\n" );
-	for( i = 0; i < 4; i++ )
-	{
-                if( !flag ){
- 		 printf( "%.2f + %.2f = ", left, right);
-                }
+  printf( "\n計算結果:\n" );
+  for( i = 0; i < 4; i++ )
+    {
+      switch( i ){
+        case 0:
+          result = left + right;
+          op = '+';
+          break;
+        case 1:
+          result = left - right;
+          op = '-';
+          break;
+        case 2:
+          result = left * right;
+          op = '*';
+          break;
+        case 3:
+          if( flag ){
+            printf("除數不得為零!!!");
+            continue;
+          }
+          else{
+            result = left / right;
+            op = '/';
+            break;
+          }
+      }
 
-		switch( i ){
-		case 0:
-			printf("%.2f", left + right);
-			break;
-		case 1:
-			printf("%.2f", left - right);
-			break;
-		case 2:
-			printf("%.2f", left * right);
-			break;
-		case 3:
-			if( flag )
-			 { printf("除數不得為零!!!"); }
-			else
-			 { printf("%.2f", left / right); }
-			break;
-		}
-		printf("\n");
-	}
-	printf("\n");
+      printf( "%.2f %c %.2f = %.2f\n", left, op, right, result);
+  }
+  printf("\n");
 
-	return 0;
+  return 0;
 }
