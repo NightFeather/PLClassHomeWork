cbc08c121f79dc04d673157b25448e7fbd83281b
NightFeather <ss900328@gmail.com>
2014-12-07 08:23:14 +0800

    Add hw3-2
    And Some modification to the Readme and hw3-1

--- hw3-1.c
+++ hw3-1.c
@@ -7,35 +7,45 @@ typedef struct {
 } time;
 
 time timeminus(time origin,time offset) {
+
   time result = {0,0,0};
+
   if(origin.sec < offset.sec) {
     origin.min --;
     origin.sec += 60;
   }
   result.sec = origin.sec - offset.sec;
+
   if(origin.min < offset.min || origin.min < 0) {
     origin.hour --;
     origin.min += 60;
   }
   result.min = origin.min - offset.min;
+
   result.hour = origin.hour - offset.hour;
+
   return result;
 }
 
 int main() {
 
-  time midnight = {23,59,59}, parsing, result;
-  int limit[3] = {24,60,60}, offset = sizeof(int), i = 0, input;
+  time midnight = {24,0,0}, ship;
+  int limit[3] = {24,60,60}, i = 0, input;
+  int* pShip[3] = { &(ship.hour),&(ship.min),&(ship.sec) };
   char tags[3][5] = {"Hour","Min","Sec"};
+
   printf("Type in the time of now\n");
   for(i = 0; i < 3; i++) {
     do {
       printf("%s: ",tags[i]);
       scanf("%d",&input);
     }while(input <0 || input >= limit[i]);
-    *((int *)(((char*)&parsing)+offset*i)) = input;
+    *pShip[i] = input;
   }
-  result = timeminus(midnight, parsing);
-  printf("Remaining: %02d:%02d:%02d\n",result.hour,result.min,result.sec);
+
+  ship = timeminus(midnight, ship);
+
+  printf("Remaining: %02d:%02d:%02d\n",ship.hour,ship.min,ship.sec);
+
   return 0;
 }
---
+++ hw3-2.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_STUDENTS 50
+
+typedef struct {
+  char name[15][20];
+  char id[8];
+  int score;
+} student;
+
+void display(student);
+void import(char*, student*,int*);
+void export(char*, student*,int);
+
+int main(){
+
+  student studentList[MAX_STUDENTS];
+  int counter = 0,i = 0;
+  char c = '\0', filename[100] = {'\0'};
+  do {
+    printf("i: 插入\nl: 列出\ns: 存檔\no: 讀檔\nq: 結束\n> ");
+    scanf("%c",&c);
+    fflush(stdin);
+    switch(c) {
+      case 'i':
+        if(counter >50){ printf("Limit Exceeded"); }
+        fflush(stdin);
+        printf("姓名: "); scanf(" %s",studentList[counter].name);
+        printf("學號: "); scanf(" %s",studentList[counter].id);
+        printf("分數: "); scanf(" %d",&(studentList[counter].score));
+        printf("插入完成!\n");
+        counter ++;
+        break;
+      case 'l':
+        for(i = 0; i<counter; i++) {
+          printf("No. %d\n",i+1);
+          display(studentList[i]);
+          printf("============\n");
+        }
+        break;
+      case 's':
+        fflush(stdin);
+        printf("輸入檔案名稱(.txt): ");
+        scanf(" %s",filename);
+        export(filename,studentList,counter);
+        break;
+      case 'o':
+        fflush(stdin);
+        printf("輸入檔案名稱(.txt): ");
+        scanf(" %s",filename);
+        import(filename,studentList,&counter);
+        break;
+      case 'q':
+        return 0;
+        break;
+      default:
+        printf("輸入錯誤!\n");
+    }
+    fflush(stdin);
+    printf("Press Enter to Continue...\n");
+    scanf("%*c");
+    fflush(stdin);
+  }while(1);
+}
+
+void display(student input) {
+  printf("姓名: %s\n",input.name);
+  printf("學號: %s\n",input.id);
+  printf("分數: %d\n",input.score);
+}
+
+
+void import(char* fname, student* list,int* pos) {
+  FILE* fp;
+  student buffer;
+  fp = fopen(fname, "r");
+  if(fp == NULL) { printf("Error Open File: \"%s\"\n",fname); return;}
+  while(fscanf(fp,"%s %s %d",buffer.name,buffer.id,&buffer.score) != EOF) {
+    list[*pos] = buffer;
+    (*pos)++;
+  }
+  fclose(fp);
+  return;
+}
+
+void export(char* fname, student* list,int size) {
+  FILE * fp;
+  int i;
+  fp = fopen(fname, "w");
+  if(fp == NULL) { printf("Error Create File: \"%s\"\n",fname); return;}
+  for(i = 0; i < size; i++) {
+    fprintf(fp, "%s %s %d \n", list[i].name, list[i].id, list[i].score );
+  }
+  fclose(fp);
+  return;
+}
